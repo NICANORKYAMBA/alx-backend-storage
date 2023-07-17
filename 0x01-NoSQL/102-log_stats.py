25ac695c2042a64beb23fa5dea6b742992232b63
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-07-17 17:02:21 +0300

    15. Log stats - new version

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon July  17 17:00:00 2023
+
+@Author: Nicanor Kyamba
+"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    """
+    Improves 12-log_stats.py by adding the top 10 of the most
+    present IPs in the collection nginx of the database logs.
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+
+    """Total number of logs"""
+    total_logs = logs_collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    """Methods count"""
+    methods = logs_collection.distinct("method")
+    print("Methods:")
+    for method in methods:
+        count = logs_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    """Status check count"""
+    status_check_count = logs_collection.count_documents(
+            {"method": "GET",
+             "path": "/status"})
+    print(f"{status_check_count} status check")
+
+    """Top 10 IPs count"""
+    top_ips = logs_collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+    print("IPs:")
+    for ip in top_ips:
+        print(f"\t{ip['_id']}: {ip['count']}")
