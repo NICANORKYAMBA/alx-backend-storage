02f6e17d3f304df701d96911a4627d8a201abc74
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-07-17 14:51:55 +0300

    10. Change school topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon July 17 14:50:00 2023
+
+@Author: Nicanor Kyamba
+"""
+from pymongo.collection import Collection
+
+
+def update_topics(mongo_collection: Collection, name: str, topics: list) -> None:
+    """
+    Updates the topics of a collection.
+
+    Args:
+        mongo_collection (Collection): The collection to update.
+        name (str): The name of the collection.
+        topics (list): The new topics of the collection.
+    """
+    mongo_collection.update_many(
+            {"name": name},
+            {"$set": {"topics": topics}}
+            )
