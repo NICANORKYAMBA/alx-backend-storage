cc9408f02ed3d1baded29961e90db38e14b9f7fd
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-07-17 14:35:07 +0300

    8. List all documents in Python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon July  17 14:22:25 2023
+
+@Author: Nicanor Kyamba
+"""
+from pymongo.collection import Collection
+from typing import List
+
+
+def list_all(mongo_collection: Collection) -> List[dict]:
+    """
+    List all documents in a collection
+
+    Args:
+        mongo_collection (Collection): collection to list
+
+    Returns:
+        List[dict]: list of documents
+    """
+    documents = []
+    for document in mongo_collection.find():
+        documents.append(document)
+    return documents
