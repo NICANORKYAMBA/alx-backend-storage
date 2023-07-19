96a9d9202d9a38f5d50979e7f4ecedb42cf69cca
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-07-19 13:28:50 +0300

    Fixed code failure

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,14 +11,14 @@ import redis
 
 
 class Cache:
-    """Initialize class Cache"""
+    """Cache class to store data in redis"""
     def __init__(self):
+        """Initialize class"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self,
-              data: Union[str, bytes, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in cache"""
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+        random_key = str(uuid.uuid4())
+        self._redis.set(random_key, data)
+        return random_key
