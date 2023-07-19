b16519dce08684c0cc03bbafd783ef32d5d1d993
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-07-19 18:59:00 +0300

    5. Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -5,63 +5,32 @@ Created on Wed July  19 19:00:00 2023
 
 @Author: Nicanor Kyamba
 """
-import time
 from functools import wraps
+import redis
 import requests
+from typing import Callable
+
+redis_ = redis.Redis()
 
 
-"""Cache dictionary to store URL responses"""
-cache:  = {}
-
-
-def get_page(url: str) -> str:
-    """
-    Check if URL response is already cached and
-    within expiration time
-    """
-    if url in cache and time.time() - cache[url]['timestamp'] < 10:
-        cache[url]['count'] += 1  # Increment access count
-        return cache[url]['content']
-
-    """Make a request to obtain the HTML content"""
-    response = requests.get(url)
-    content = response.text
-
-    """Cache the response with access count and timestamp"""
-    cache[url] = {
-        'content': content,
-        'count': 1,
-        'timestamp': time.time()
-    }
-
-    return content
-
-
-def cache_and_track(func):
-    """Decorator to cache and track URL accesses"""
-    @wraps(func)
-    def wrapper(url):
-        """
-        Check if URL response is already cached
-        and within expiration time
-        """
-        if url in cache and time.time() - cache[url]['timestamp'] < 10:
-            cache[url]['count'] += 1
-            return cache[url]['content']
-        else:
-            content = func(url)
-            cache[url] = {
-                'content': content,
-                'count': 1,
-                'timestamp': time.time()
-            }
-            return content
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting """
+    @wraps(method)
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
 
     return wrapper
 
 
-@cache_and_track
-def get_page_with_decorator(url: str) -> str:
-    """Using the decorator to implement get_page function"""
-    response = requests.get(url)
-    return response.text
+@count_requests
+def get_page(url: str) -> str:
+    """ Obtain the HTML content of a  URL """
+    req = requests.get(url)
+    return req.text
